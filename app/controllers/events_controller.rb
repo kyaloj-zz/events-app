55a1a284644549cea5561c3cf1e3128cc7d9027d
justus <jmbaluka@mfarm.co.ke>
2015-09-10 08:56:47 +0300

    restricting event manipulation to owners only

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :event_owner!, only: [:edit, :update, :destroy]
 
   # GET /events
   # GET /events.json
@@ -71,5 +72,14 @@ class EventsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:event).permit(:title, :start_date, :end_date, :location, :agenda, :address, :organizer_id)
+    end
+
+    def event_owner!
+     authenticate_user!
+      if @event.organizer_id != current_user.id
+        redirect_to events_path
+        flash[:notice] = 'You do not have enough
+        permissions to do this'
+      end
     end
 end
